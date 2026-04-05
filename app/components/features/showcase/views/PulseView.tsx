b79c782e67e98aba078990fc34c19d3027d66ac4
Maxime <pro.maximebreton@gmail.com>
2026-04-05 20:43:49 +0200

    -

--- app/components/features/showcase/views/PulseView.tsx
+++ app/components/features/showcase/views/PulseView.tsx
@@ -1,7 +1,12 @@
 export default function PulseView() {
-    return(
-        <div>
-            
+    return (
+        <div className="flex flex-col">
+            <div>
+                <div>Initiatives</div>
+                <div className="bg-hovered">For me</div>
+                <div className="bg-hovered">Popular</div>
+                <div className="bg-hovered">Recent</div>
+            </div>
         </div>
-    )
+    );
 }
